8022d46e12e8abdcc60a0e1149b86d9c56e119c4
Vanessa Rose Reyes <vareyes@blizzard.com>
2022-12-14 13:14:15 -0700

    Added database connection, added past travel pages, able to query database and display data on webapge

--- assets/PastTravels.js
+++ assets/PastTravels.js
@@ -43,21 +43,51 @@ function CreateCard(place, description, rowCount) {
     row.appendChild(col)
 }
 
+function EmptyDisplay(container) {
+    // Create div for content
+    div = document.createElement('div')
+    div.setAttribute('id', 'emptyContent')
+
+    // Create text
+    text = document.createElement('h4')
+    text.innerHTML = "Looks like you don't have any past travels. Let's start building your journal by putting your past travels!"
+    
+    // Create button
+    button = document.createElement('a')
+    button.setAttribute('href', 'http://localhost:400/PastTravelForm')
+    button.setAttribute('role', 'button')
+    button.setAttribute('class', 'btn btn-dark btn-lg')
+    button.innerHTML = 'Get Started'
+    
+    // Add elements to div and container
+    div.appendChild(text)
+    div.appendChild(button)
+    container.appendChild(div)
+
+    // TODO: add some images to take up some space
+}
+
 // get cards for next travel ideas page
 function LoadCards() {
-    fetch("/FutureTravelCards")
+    fetch("/PastTravelsCards")
     .then(response => response.text())
     .then(text => {
         var container = document.getElementById("cardContainer")
         try {
             const travelList = JSON.parse(text) // Try to parse the response as JSON
-            console.log(travelList)
+            console.log("Travel list:" + travelList)
+            console.log("Length: " + (travelList).length)
+            if ((travelList).length == 0) {
+                console.log("here")
+                EmptyDisplay(container)
+                return
+            }
             var header = document.getElementById('headerContent')
             var button = document.createElement('a')
-            button.setAttribute('href', 'http://localhost:400/NextTravelForm')
+            button.setAttribute('href', 'http://localhost:400/PastTravelForm')
             button.setAttribute('role', 'button')
             button.setAttribute('class', 'btn btn-dark')
-            button.innerHTML = "Add New Travel Idea"
+            button.innerHTML = "Add Past Travel"
             header.appendChild(button)
 
             // response was JSON
@@ -82,41 +112,13 @@ function LoadCards() {
                 colCount++
             })
         } catch(err) {
-            // check if there is only one object
-            if ((text).length > 0) {
-                const travel = JSON.parse(text) // Try to parse the response as JSON
-                var row = document.createElement('div')
-                row.setAttribute('class', 'row')
-                row.setAttribute('id', 'cardRow' + rowCount)
-                container.appendChild(row)
-                CreateCard(travel.place, travel.description, rowCount)
-            }
-            else {
-                // The response wasn't a JSON object
-
-                // Create div for content
-                div = document.createElement('div')
-                div.setAttribute('id', 'emptyContent')
-
-                // Create text
-                text = document.createElement('h4')
-                text.innerHTML = "Looks like you don't have any future travel plans. Let's start building your journal and plan a travel!"
-                
-                // Create button
-                button = document.createElement('a')
-                button.setAttribute('href', 'http://localhost:400/NextTravelForm')
-                button.setAttribute('role', 'button')
-                button.setAttribute('class', 'btn btn-dark btn-lg')
-                button.innerHTML = 'Get Started'
-                
-                // Add elements to div and container
-                div.appendChild(text)
-                div.appendChild(button)
-                container.appendChild(div)
-
-                // TODO: add some images to take up some space
-
-            }
+            // JSON was only one object
+            const travel = JSON.parse(text) // Try to parse the response as JSON
+            var row = document.createElement('div')
+            row.setAttribute('class', 'row')
+            row.setAttribute('id', 'cardRow' + rowCount)
+            container.appendChild(row)
+            CreateCard(travel.place, travel.description, rowCount)
         }
     })
 }
--- main.go
+++ main.go
@@ -26,30 +26,37 @@ import (
 )
 
 var port = 400
+var DB *sql.DB = nil
 
 type NewTravelItem struct {
+	ID          int    `json:"id"`
 	Place       string `json:"place"`
 	Description string `json:"description"`
 }
 
 var nextTravels = []NewTravelItem{
 	NewTravelItem{
+		ID:          1,
 		Place:       "Europe",
 		Description: "I want to go here",
 	},
 	NewTravelItem{
+		ID:          2,
 		Place:       "Las Vegas",
 		Description: "I want to go here",
 	},
 	NewTravelItem{
+		ID:          3,
 		Place:       "Europe",
 		Description: "I want to go here",
 	},
 	NewTravelItem{
+		ID:          4,
 		Place:       "Las Vegas",
 		Description: "I want to go here",
 	},
 	NewTravelItem{
+		ID:          5,
 		Place:       "Europe",
 		Description: "I want to go here",
 	},
@@ -58,7 +65,7 @@ var nextTravels = []NewTravelItem{
 func ConnectToDB() *sql.DB {
 	// The `sql.Open` function opens a new `*sql.DB` instance. We specify the driver name
 	// and the URI for our database. Here, we're using a Postgres URI
-	db, err := sql.Open("pgx", "user=postgres password=2400 host=localhost port=5432 dbname=travel-db sslmode=verify-ca pool_max_conns=10")
+	db, err := sql.Open("pgx", "user=postgres password=2400 host=localhost port=5432 dbname=travel-db sslmode=disable")
 	if err != nil {
 		log.Fatalf("could not connect to database: %v", err)
 	}
@@ -77,19 +84,22 @@ func main() {
 	r := mux.NewRouter()
 
 	// connect to DB
-	// ConnectToDB()
+	DB = ConnectToDB()
 
 	// define handlers
 	r.HandleFunc("/", mainPageHandler).Methods("GET")
 
 	// Future Travels
 	r.HandleFunc("/NextTravelIdeas", getNextTravelIdeasPage).Methods("GET")
-	r.HandleFunc("/TravelIdeasCards", getNextTravelCards).Methods("GET")
+	r.HandleFunc("/FutureTravelCards", getNextTravelCards).Methods("GET")
 	r.HandleFunc("/NextTravelForm", getNextTravelFormPage).Methods("GET")
 	r.HandleFunc("/SubmitNewTravelIdea", postNewTravel).Methods("POST")
 
 	// Past Travels
 	r.HandleFunc("/PastTravels", getPastTravelsPage).Methods("GET")
+	r.HandleFunc("/PastTravelsCards", getPastTravelCards).Methods("GET")
+	r.HandleFunc("/PastTravelForm", getPastTravelFormPage).Methods("GET")
+	r.HandleFunc("/SubmitPastTravel", postPastTravel).Methods("POST")
 
 	// Declare the handler, that routes requests to their respective filename.
 	// The fileserver is wrapped in the `stripPrefix` method, because we want to
@@ -114,8 +124,29 @@ func getNextTravelIdeasPage(w http.ResponseWriter, r *http.Request) {
 }
 
 func getNextTravelCards(w http.ResponseWriter, r *http.Request) {
+	data, err := DB.Query("SELECT * FROM future_travels")
+	if err != nil {
+		log.Fatalf("could not execute query: %v", err)
+	}
+
+	// create array to hold data
+	futureTravels := []NewTravelItem{}
+
+	// iterate over the returned rows
+	// we can go over to the next row by calling the `Next` method, which will
+	// return `false` if there are no more rows
+	for data.Next() {
+		travel := NewTravelItem{}
+		// create an instance of `Bird` and write the result of the current row into it
+		if err := data.Scan(&travel.ID, &travel.Place, &travel.Description); err != nil {
+			log.Fatalf("could not scan row: %v", err)
+		}
+		// append the current instance to the slice of birds
+		futureTravels = append(futureTravels, travel)
+	}
+
 	// Convert the "nextTravels" variable to json
-	travelListBytes, err := json.Marshal(nextTravels)
+	travelListBytes, err := json.Marshal(futureTravels)
 
 	// If there is an error, print it to the console, and return a server
 	// error response to the user
@@ -150,19 +181,137 @@ func postNewTravel(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
+	// TODO: need to have better way of giving ids
+	row := DB.QueryRow("SELECT COUNT(*) FROM future_travels")
+	if err := row.Scan(&newTravel.ID); err != nil {
+		log.Fatalf("could not scan row: %v", err)
+	}
+
 	// Get the information about the travel from the form info
 	newTravel.Place = r.Form.Get("place")
 	newTravel.Description = r.Form.Get("description")
+	newTravel.ID++ // increment id for new item
 
-	// Append our existing list of travel with a new entry
-	nextTravels = append(nextTravels, newTravel)
+	fmt.Print(newTravel)
+
+	// the `Exec` method returns a `Result` type instead of a `Row`
+	// we follow the same argument pattern to add query params
+	result, err := DB.Exec("INSERT INTO future_travels (id, place, description) VALUES ($1, $2, $3)", newTravel.ID, newTravel.Place, newTravel.Description)
+	if err != nil {
+		log.Fatalf("could not insert row: %v", err)
+	}
+
+	// the `Result` type has special methods like `RowsAffected` which returns the
+	// total number of affected rows reported by the database
+	// In this case, it will tell us the number of rows that were inserted using
+	// the above query
+	rowsAffected, err := result.RowsAffected()
+	if err != nil {
+		log.Fatalf("could not get affected rows: %v", err)
+	}
+	// we can log how many rows were inserted
+	fmt.Println("inserted", rowsAffected, "rows")
 
 	//Finally, we redirect the user to the original HTMl page
 	// (located at `/assets/`), using the http libraries `Redirect` method
-	http.Redirect(w, r, "http://localhost:400", http.StatusFound)
+	http.Redirect(w, r, "http://localhost:400/NextTravelIdeas", http.StatusFound)
 }
 
 func getPastTravelsPage(w http.ResponseWriter, r *http.Request) {
 	t, _ := template.ParseFiles("./assets/PastTravels.html")
 	t.Execute(w, nil)
+}
+
+func getPastTravelCards(w http.ResponseWriter, r *http.Request) {
+	data, err := DB.Query("SELECT * FROM past_travels")
+	if err != nil {
+		log.Fatalf("could not execute query: %v", err)
+	}
+
+	// create array to hold data
+	pastTravels := []NewTravelItem{}
+
+	// iterate over the returned rows
+	// we can go over to the next row by calling the `Next` method, which will
+	// return `false` if there are no more rows
+	for data.Next() {
+		travel := NewTravelItem{}
+		// create an instance of `Bird` and write the result of the current row into it
+		if err := data.Scan(&travel.ID, &travel.Place, &travel.Description); err != nil {
+			log.Fatalf("could not scan row: %v", err)
+		}
+		// append the current instance to the slice of birds
+		pastTravels = append(pastTravels, travel)
+	}
+
+	// Convert the "nextTravels" variable to json
+	travelListBytes, err := json.Marshal(pastTravels)
+
+	// If there is an error, print it to the console, and return a server
+	// error response to the user
+	if err != nil {
+		fmt.Println(fmt.Errorf("Error: %v", err))
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
+	// If all goes well, write the JSON list of birds to the response
+	w.Write(travelListBytes)
+}
+
+func getPastTravelFormPage(w http.ResponseWriter, r *http.Request) {
+	t, _ := template.ParseFiles("./assets/PastTravelForm.html")
+	t.Execute(w, nil)
+}
+
+func postPastTravel(w http.ResponseWriter, r *http.Request) {
+	// Crate a new travel object
+	pastTravel := NewTravelItem{}
+
+	// We send all our data as HTML form data
+	// the `ParseForm` method of the request, parses the
+	// form values
+	err := r.ParseForm()
+
+	// In case of any error, we respond with an error to the user
+	if err != nil {
+		fmt.Println(fmt.Errorf("Error: %v", err))
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
+	// TODO: need to have better way of giving ids
+	row := DB.QueryRow("SELECT COUNT(*) FROM past_travels")
+	if err := row.Scan(&pastTravel.ID); err != nil {
+		log.Fatalf("could not scan row: %v", err)
+	}
+
+	// Get the information about the travel from the form info
+	pastTravel.Place = r.Form.Get("place")
+	pastTravel.Description = r.Form.Get("description")
+	pastTravel.ID++ // increment id for new item
+
+	fmt.Print(pastTravel)
+
+	// the `Exec` method returns a `Result` type instead of a `Row`
+	// we follow the same argument pattern to add query params
+	result, err := DB.Exec("INSERT INTO past_travels (id, place, description) VALUES ($1, $2, $3)", pastTravel.ID, pastTravel.Place, pastTravel.Description)
+	if err != nil {
+		log.Fatalf("could not insert row: %v", err)
+	}
+
+	// the `Result` type has special methods like `RowsAffected` which returns the
+	// total number of affected rows reported by the database
+	// In this case, it will tell us the number of rows that were inserted using
+	// the above query
+	rowsAffected, err := result.RowsAffected()
+	if err != nil {
+		log.Fatalf("could not get affected rows: %v", err)
+	}
+	// we can log how many rows were inserted
+	fmt.Println("inserted", rowsAffected, "rows")
+
+	//Finally, we redirect the user to the original HTMl page
+	// (located at `/assets/`), using the http libraries `Redirect` method
+	http.Redirect(w, r, "http://localhost:400/PastTravels", http.StatusFound)
 }
